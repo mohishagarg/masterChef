c73f8d6db2aab13efb8247a62a9dcc93c50c0bfc
mohishagarg <82024289+mohishagarg@users.noreply.github.com>
2021-05-10 13:45:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var background,backgroundImg;
+var chocolateCake;
+
+function preload(){
+  backgroundImg=loadImage("backgroundImage.png");
+}
+
+function setup(){
+  createCanvas(3000,2000);
+}
+
+function draw(){
+  background(backgroundImg)
+  textSize(25);
+  fill("blue");
+  text("CHOCOLATE CAKE RECIPE",100,50);
+  fill("black");
+  text("INGREDIENTS",100,90);
+  fill("red");
+  text("=> Cocoa Powder",50,130)
+  text("=>Whole Wheat Flour",50,170)
+text("=>Baking Soda",50,210)
+text("=>Lemon Juice",50,250)
+text("=>Oil",50,290)
+text("=>Flavourings(vanilla extract)",50,330)
+text("=>liquids(liquid of raw sugar and water)",50,370)
+fill("black")
+text("RECIPE",100,770)
+fill("magenta");
+text("1. First grease a round baking pan of 7.5 inches diameter x 2 inches height with oil. Preheat your oven to 200 degrees Celsius (392 degrees Fahrenheit) for 15 minutes.",50,810)
+text("2. Sieve 1 cup whole wheat flour (you can also use all-purpose flour) and 3 tablespoons cocoa powder. Keep a plate or tray below the sieve.",50,850)
+text("3. Add a pinch of salt and ½ teaspoon baking soda.",50,890)
+text("4. Sift the ingredients on to a plate.",50,930)
+text("5. Keep the sieved dry ingredients aside.",50,970)
+text("6. In another pan or bowl, add ¾ cup sugar. I have used raw, unrefined cane sugar, however regular white sugar can also be used. Add 1 cup cold water. The water has to be very cold.",50,1010)
+text("7. Stir with a small whisk till the sugar dissolves.",50,1050)
+text("8. Now add ¼ cup oil. I added sunflower oil..Note: I would not suggest adding butter as a few readers did tell me that the butter solidifies and it becomes difficult to get it to a homogenous mixture. Stick with oils that are not solid at room temperature for this cake.",50,1090)
+text("9. Stir briskly so that everything is mixed well..It should be a homogenous mixture.",50,1130)
+text("11. Next add 1 tablespoon lemon juice. Stir again...TIP: No lemon? No problem. We just need something acidic here. You could use 1 tablespoon of apple cider vinegar or white vinegar in place of lemon juice.",50,1170)
+text("12. Now add ½ teaspoon vanilla extract. Feel free to add chocolate extract or chocolate essence instead of vanilla.",50,1210)
+text("13. Mix very well.",50,1250)
+text("14. Add the sieved dry ingredients to the wet mixture. ",50,1290)
+text("15. Using a wired whisk, mix everything well. ",50,1330)
+text("16. There should be no lumps in the cake batter. The batter should also be slightly thin, not thick like a brownie batter. ",50,1370)
+text("17.Pour the eggless chocolate cake batter into the prepared cake pan..",50,1410)
+text("18. Tap the sides so that the extra air bubbles are let out.",50,1450)
+text("19. Bake the eggless chocolate cake in a preheated oven for 200 degrees Celsius (392 degrees Fahrenheit) for 35 to 40 mins.",50,1490)
+text("20. For doneness check the chocolate cake with a bamboo skewer or toothpick. The bamboo skewer should come out clean....If the toothpick is sticky with the batter, then place the chocolate cake back in the oven and continue to bake for some more minutes..Once the eggless chocolate cake cools down enough to handle, remove it from the baking tin and place it on a wired rack to finish cooling. Before frosting, let the cake cool completely.",50,1530)
+text("21. This frosting recipe makes for more of a thin chocolate glaze. Take 1 tablespoon sunflower oil, ¼ cup almond milk and ¼ cup raw sugar in a small saucepan.",50,1570)
+text("22. Begin to heat the pan on a low flame. Mix very well, continuing to stir, so that the sugar is dissolved into the milk. Do not boil the almond milk, but just make it lightly hot enough to dissolve the sugar.",50,1610)
+text("23. When the almond milk is just lightly hot and after all the sugar has been dissolved, add 3 tablespoons cocoa powder.",50,1650)
+text("24. With a small wired whisk, mix and stir so that the cocoa powder gets mixed evenly.",50,1690)
+text("25. The cocoa frosting is ready now. Switch off the flame.",50,1730)
+text("26. While still hot, pour the frosting on the top and sides of the eggless chocolate cake...TIP: Usually I wrap a medium sized plate with aluminum foil and then place the cake on it. This makes the frosting work neater.",50,1770)
+text("28. For a little added pizzazz, feel free to top your cake with some simple decor. I recommend chocolate shavings, sprinkles or sliced nuts. If they are in season, fresh berries are also a nice complement to chocolate cake...If you want to make a more intricate design, try holding a doily over the top of the cake and sift icing sugar or cocoa powder over the top to look like lace.",50,1810)
+text("The eggless chocolate cake is ready to be served. Slice and enjoy!....Serve the chocolate cake as it is as an after meal dessert or whenever the chocolate craving strikes! It is simple enough to enjoy with your afternoon tea, and decadent enough to serve at a dinner party....",50,1850)
+
+}
